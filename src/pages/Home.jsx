340b8348538fe683fa4ef38008e51308732faff7
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-07-15 17:51:37 +0600

    created a reusable Container component to avoid repeating layout classes throughout the application

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import "react";
+import Container from "../components/common/Container";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home page</h1>
-        </div>
+        <Container className="py-20">
+            <h1 className="text-4xl font-bold">Welcome to Loomify</h1>
+            <p className="mt-4">fgs;ggjgsjgj </p>
+        </Container>
     );
 };
 
