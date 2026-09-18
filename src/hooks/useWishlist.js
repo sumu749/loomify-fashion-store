89c02d96a353ad0dacbbdb3e8d0006b7119993ac
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-09-18 23:21:09 +0600

    Remove unused legacy Vite app

--- src/hooks/useWishlist.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { WishlistContext } from "../context/WishlistContext";
-
-const useWishlist = () => useContext(WishlistContext);
-
-export default useWishlist;
